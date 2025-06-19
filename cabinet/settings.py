693ac321df69b3d191229a2718683bb067b77f37
Philippe Mourey <philippe.mourey@gmail.com>
2025-06-19 17:43:41 +0200

    add strava app and middleware

--- cabinet/settings.py
+++ cabinet/settings.py
@@ -16,7 +16,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'django.contrib.humanize', # format your numbers with thousand separators.
     'medecin',
+    'strava'
 ]
 
 MIDDLEWARE = [
@@ -27,6 +29,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cabinet.middleware.RoleBasedMiddleware',
 ]
 
 ROOT_URLCONF = 'cabinet.urls'
